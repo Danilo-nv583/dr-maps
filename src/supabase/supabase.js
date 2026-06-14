76f43b02d303c7c95e9d9e64d75ceb1d405e5214
Danilo-nv583 <16.rodriguezdanilo2006@gmail.com>
2026-06-14 16:59:10 -0600

    Configurar variables de entorno

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lttzpuittdmhqlwqalii.supabase.co';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-const supabaseKey =
-  'sb_publishable_iGesvgYbJ30kNepJmUjnEA_sHNWR4_7';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
