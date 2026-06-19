8a8a02488de31dceb820e0b6ecb96cb8223a5f8c
Danilo-nv583 <16.rodriguezdanilo2006@gmail.com>
2026-06-19 00:15:18 -0600

    Convertir DR Maps en PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'DR Maps',
+        short_name: 'DR Maps',
+        description: 'Sistema de control de lotes para lotificadoras',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+
+        icons: [
+          {
+            src: '/icons.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: '/icons.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
 })
